d77b404a7567ece033d98b6c9c3e9467414bf0a0
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2023-05-28 15:34:30 -0500

    Se agregó el archivo JavaSyntax.java

---
+++ JavaSyntax.java
@@ -0,0 +1,16 @@
+// Una vez se compile el archivo ".java", el código que se ejecuta en Java está dentro de un archivo ".class".
+
+// Una clase siempre debe comenzar con una primera letra mayúscula.
+public class JavaSyntax {
+
+    // El main() es un método obligatorio y lo tienen todo los programas en Java. Las llaves marcan el inicio y final de un código
+    public static void main(String[]args) {
+
+        // Podemos usar println() para imprimir una linea de texto en pantalla. Cada instrucción de código debe terminar con ";".
+        System.out.println("Hola mundo!");
+
+    }
+    
+}
+
+// Para comentar un código en Java se usa "//" si se quiere comentar en una linea. Para comentar en más lineas use "/*" al inicio y "*/" al final.
