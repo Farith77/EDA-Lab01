cd720002ae57a8646acc570db8a0f9f29a7a0b5a
Farith <agarciaa@unsa.edu.pe>
2023-05-28 12:56:58 -0500

    Agregando el archivo Alcance

---
+++ Alcance.java
@@ -0,0 +1,14 @@
+public class Alcance{
+	
+	public static void main(String[] args) {
+		//En esta parte no existe x
+		// Aqui no puede usarse x
+
+		int x = 100;
+
+		// Aqui ya existe x por lo que si se puede usar
+		System.out.println(x);
+	} 
+	//sobra decir que en otros meodos no se puede usar x
+	//por que solo pertenece al main
+}
