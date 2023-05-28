47fa9d9a393221f6b5317ae056742226c530b0fe
Christian Venero Guevara <cvenero@unsa.edu.pe>
2023-05-28 14:16:23 -0500

    Creacion de metodos

---
+++ JavaMethods.java
@@ -0,0 +1,12 @@
+public class JavaMethods {
+	//Metodo que imprime un texto
+	static void myMethod() {
+		System.out.println("I just got executed!");
+	}
+
+	public static void main(String[] args) {
+		//Los metodos puedes ser llamados una o varias veces
+		myMethod();
+	    myMethod();
+	}
+}
