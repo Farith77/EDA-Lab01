30c95c7dd6b03c4d5eccf8515286ca70b5488c42
Cristian Raul Saya Vargas <csaya@unsa.edu.pe>
2023-05-28 16:36:52 -0500

    concatenacion de cadenas

---
+++ String/concatenar.java
@@ -0,0 +1,11 @@
+package String;
+
+public class concatenar {
+    public static void main(String []args){
+        //concatenamos dos cadenas 
+        String firstName = "John";
+        String lastName = "Doe";
+        System.out.println(firstName + " " + lastName);
+    }
+    
+}
