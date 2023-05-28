047c2a59cb9eda3365d05ffdfc7454f4a6a27313
Cristian Raul Saya Vargas <csaya@unsa.edu.pe>
2023-05-28 16:42:48 -0500

    caracteres especiales

---
+++ String/caractEs.java
@@ -0,0 +1,11 @@
+package String;
+
+public class caractEs {
+    public static void main(String[]args){
+        //Usamoa  / / para poder poner caracteres especiales como las comillas
+        String txt = "We are the so-called \"Vikings\" from the north.";
+
+        System.out.println(txt);
+    }
+    
+}
