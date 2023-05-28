48f73d010ea090d1fd6a2eb97e9b7b437556cbb8
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2023-05-28 14:59:11 -0500

    Se agregó el archivo JavaIntro.java

---
+++ JavaIntro.java
@@ -0,0 +1,23 @@
+public class JavaIntro {
+
+    //¿Qué es Java?
+
+    /*  
+     * Java es un popular lenguaje de programación creado en 1995.
+     * Entre sus principales utilidades estan:
+     * - Aplicaciones móviles(principalmente en Android).
+     * - Aplicaciones de Escritorio y web.
+     * - Servidores web y conexión de base de datos.
+     * - Juegos y mucho más.
+    */
+
+    //¿Por qué usar Java?
+
+    /*
+     * - Java funciona en diferentes plataformas(Windows, Mac, Linux, etc.)
+     * - Es uno de los lenguajes más populares del mundo y tiene gran demanda en el mercado laboral actual.
+     * - Es facil de aprender y usar, además es de código abierto y gratuito.
+     * - Es un lenguaje orientado a objetos, con estructura clara y permite reutilizar el código. 
+     */
+
+}
