0c155a1a56d4f7b444946f5aa5aa78ea39fc8c23
avillaq <villafuertequispealex@gmail.com>
2023-05-28 14:41:12 -0500

    Demostracion de los comentarios en una y varias lineas

---
+++ CommentsJava.java
@@ -0,0 +1,17 @@
+public class CommentsJava{
+    public static void main(String[] args) {
+
+        // Comentarios de una sola linea
+        // Imprimir "Hello world"
+        System.out.println("Hello World");
+
+
+        /* Comentarios de varias lineas
+
+         * El siguiente codigo imprime
+         * en consola en la clasico "Hello World" 
+         */
+        System.out.println("Hello World");
+
+    }
+}
