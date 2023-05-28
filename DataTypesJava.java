80eb0dd2d64087ead85126161722a881fdfbaff7
avillaq <villafuertequispealex@gmail.com>
2023-05-28 14:58:41 -0500

    Vemos los diferentes tipos de datos y se almacenan en una variable

---
+++ DataTypesJava.java
@@ -0,0 +1,24 @@
+public class DataTypesJava {
+    public static void main(String[] args) {
+        // Tipos de datos primitivos mas usados 
+
+        //int
+        int intNum = 5;
+
+        //double
+        double doubleNum = 5.99;
+
+        //char
+        char charLetter = 'D'; 
+
+        //boolean
+        boolean bool = true; 
+
+        //El String no es un tipo de dato primitivo
+        //pero es tambien muy usado
+        
+        //String
+        String text = "Hello"; 
+
+    }
+}
