8eabf5ed59ffb262550bc106e5dd804a6c06742b
LuisArocutipa <69124601+LuisArocutipa@users.noreply.github.com>
2023-05-28 15:21:57 -0500

    Se agregó el archivo JavaGetStarted.java

---
+++ JavaGetStarted.java
@@ -0,0 +1,21 @@
+// Instalación de Java
+
+/*
+ * - Primero verifica si tienes instalado Java en tu PC, para esto utilizamos el comando "java -version" en la terminal.
+ * - Si esta instalado, se mostrará un mensaje con la versión de Java con la que se cuenta.
+ * - Si no esta instalado, puede descargarlo gratis en "oracle.com". Además puede usar un entrono de desarrollo como Netbeans o Eclipse.
+ * - Asegurece de configurar Java cuando lo instale, asegurandose que en las Variables de Entronon tenga la ruta correcta en la que se haya instalado Java. 
+ * - Una vez que cumpla los requisitos anteriores podrá empezar a usar Java.
+ */
+
+// El nombre de la clase debe coincidir con el nombre del archivo, JavaGetStarted en este caso.
+public class JavaGetStarted { 
+
+    public static void main(String[]args) {
+        System.out.println("Hola mundo!");
+    }
+
+}
+// El código del archivo muestra el mensaje "Hola mundo!".
+// Para ejecutarlo primero compile el archivo utilizando el comando "javac JavaGetStarted.java" en al terminal.
+// Luego ejecute el archivo utilizando el código "java JavaGetStarted", enseguida se mostrara el la terminal el mensaje del archivo.
