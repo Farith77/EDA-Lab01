7f234a1fd618db013c4901610e863541680aa68f
Christian Venero Guevara <cvenero@unsa.edu.pe>
2023-05-28 13:02:27 -0500

    Método whule y do-while con un ejemplo base

---
+++ JavaWhileLoop.java
@@ -0,0 +1,19 @@
+public class JavaWhileLoop {
+	public static void main(String[] args) {
+		
+		//Bucle While
+		int i = 0;
+		while (i < 5) {
+			System.out.println(i);
+			i++;
+		}
+		
+		//Bucle Do-WHile
+		int j = 0;
+		do {
+			System.out.println(j);
+			j++;
+		} while (j < 5);
+	}
+
+}
